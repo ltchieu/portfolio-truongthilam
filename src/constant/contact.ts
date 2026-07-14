e59bea404d44751740b950a37257e07257932091
ltchieu <ltchieu>
2026-07-14 15:06:23 +0700

    add link facebook

--- src/constant/contact.ts
+++ src/constant/contact.ts
@@ -3,6 +3,6 @@ import { ContactDetail } from "@/model/contact";
 export const CONTACT_INFO: ContactDetail = {
   email: "Ttuelam1310@gmail.com",
   facebook: "Trương Tuệ Lâm",
-  facebookUrl: "https://www.facebook.com/search/top?q=Tr%C6%B0%C6%A1ng%20Tu%E1%BB%87%20L%C3%A2m",
+  facebookUrl: "https://www.facebook.com/share/1DAZLUaoBp/?mibextid=wwXIfr",
   phone: "0963695843",
 };
